662d55115c58996b2dde53267ff34ec5094ef679
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-22 08:36:12 -0700

    wrote some parameters for bitcoin vs bitcoin cash

---
+++ snap-overstock.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 1/22/2018
+ * Time: 8:06 AM
+ */
+/**write a function to make a purchase in birtcoin cash and deduct from a user's bitcoin wallet
+ * input:(float) price in bitcoin
+ * input: (float) user's bitcoin wallet balance
+ * input: (float) user's new bitcoin wallet balance
+ * use case:
+ * 0)user purchases an item and pays with bitcoin
+ * 1)overstock only charges equivalent bitcoin cash
+ * 2)user's wallet is deducted a lower amount in bitcoin
+ **/
+//const priceInBitcoin = float($priceInBitcoin);
+//const priceInBitcoinCash = float($priceInBitcoin * 0.22)
+/**
+ * bitcoin's value
+ *
+ * @param float $priceInBitcoin
+ *
+ */
+/**
+ * bitoin cash's value in relation to bitcoin
+ *
+ * @param float $priceInBitoinCash
+ */
+/**
+ * user's wallet balance
+ * @param float $userWalletBefore
+ * @param float $userWalletAfter
+**/
+
+$userWalletBefore - $priceInBitcoinCash = $userWalletAfter
+$priceInBitcoin = float(0.0)
+priceInBitcoinCash = float($priceInBitcoin * 0.22)
+
+	function
+
+
+$var = 0.22;
+$float_value_of_var = floatval($var);
+echo $float_value_of_var; // 122.34343
+?>
